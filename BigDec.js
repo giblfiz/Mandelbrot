a5702d1377c12ce7b10967130334227a72956d00
Harry Pottash <giblfiz@gmail.com>
2010-06-30 09:10:57 -0700

    The bigDec library is now basicaly functional, it passes most of it's critical tests, and probably ready for use

--- BigDec.js
+++ BigDec.js
@@ -47,22 +47,66 @@ function bigDec(rawVal){
 	}
     }
 
+
+    function _ltNoSign(input){
+	input = _isABigDec(input) ? input : bigDec(input);
+	var A = mult(_numerator, input.getDenominator());
+	var B = mult(_denominator, input.getNumerator());
+	return greater(B,A) ? true: false;
+    }
+    
+    function _plus(input, subtraction){
+	input = _isABigDec(input) ? input : bigDec(input);
+	var inputIsNegitive = input.isNegitive();
+	if (subtraction){
+	    inputIsNegitive == !inputIsNegitive;
+	}
+
+	if(_isNegitive == inputIsNegitive){
+	    _numerator = add(mult(_numerator,input.getDenominator()),
+			     mult(_denominator, input.getNumerator()));
+	} else {
+	    if (_ltNoSign(input)){
+		_numerator =  sub(mult(_denominator, input.getNumerator()),
+				  mult(_numerator,input.getDenominator()));
+		_isNegitive = inputIsNegitive;
+	    } else {
+		_numerator = sub(mult(_numerator,input.getDenominator()),
+				 mult(_denominator, input.getNumerator()));
+	    }
+	}
+	_denominator = mult(_denominator, input.getDenominator());
+	return this;
+	
+    }
+
+
     if(_isABigDec(rawVal)){
 	return(rawVal);
     } else {
 	if(typeof(rawVal) == "string"){
-	    var newVal = parseFloat(rawVal);
-	    _numerator = str2bigInt(rawVal.replace(".",""),10,1,1);
+	    if(parseFloat(rawVal) < 0 ){
+		_isNegitive = true;
+		_numerator = sub(int2bigInt(0,1,1), str2bigInt(rawVal.replace(".",""),10,1,1));
+	    } else {
+		_numerator = str2bigInt(rawVal.replace(".",""),10,1,1);
+	    }
 	    _denominator = int2bigInt(Math.pow(10,_getNumberOfDecimalPoints(rawVal)),1,1);
 	} else if (typeof(rawVal) == "number"){
+	    if(rawVal < 0){
+		_isNegitive = true;
+		rawVal *= -1;
+	    }
 	    _numerator = int2bigInt((rawVal * Math.pow(10, _getNumberOfDecimalPoints(rawVal))) ,1,1);
 	    _denominator = int2bigInt(Math.pow(10,_getNumberOfDecimalPoints(rawVal)),1,1);
 	} else if (typeof(rawVal) == "object"){
 	    //for the moment we are going to assume that any array we are passed 
 	    // is actually a bigInt. This will obviously go away once the bigInt lib is refactored
+	    if (negative(rawVal)){ throw RangeError ;}
 	    _numerator = rawVal;
 	    _denominator = int2bigInt(1,1,1);
-	} 
+	}
+
 
 	return({
 	    isA:"bigDec",
@@ -75,22 +119,61 @@ function bigDec(rawVal){
 		return(_denominator);
 	    },
 	    
+	    print: function(){
+		var negSign = _isNegitive ? "-" : "";
+		return(negSign + bigInt2str(_numerator, 10) + "/"+ bigInt2str(_denominator,10));
+	    },
+
 	    eq:function(input){
 		input = _isABigDec(input) ? input : bigDec(input);
 		var A = mult(_numerator, input.getDenominator());
 		var B = mult(_denominator, input.getNumerator());
-		if(equals(A,B)){
+		return equals(A,B) ?  true : false;
+	    },
+
+	    gt:function(input){
+		input = _isABigDec(input) ? input : bigDec(input);
+		var A = mult(_numerator, input.getDenominator());
+		var B = mult(_denominator, input.getNumerator());
+		return greater(A,B) ? true: false;
+	    },
+
+	    lt:function(input){
+		input = _isABigDec(input) ? input : bigDec(input);
+		var A = mult(_numerator, input.getDenominator());
+		var B = mult(_denominator, input.getNumerator());
+		return greater(B,A) ? true: false;
+	    },
+
+	    gte:function(input){
+		input = _isABigDec(input) ? input : bigDec(input);
+		var A = mult(_numerator, input.getDenominator());
+		var B = mult(_denominator, input.getNumerator());
+		if(greater(A,B) || equals(A,B)){
 		    return true;
 		} else{
 		    return false;
 		}
 	    },
-	    
-	    plus: function(input){
+
+	    lte:function(input){
 		input = _isABigDec(input) ? input : bigDec(input);
-		_numerator = add(mult(_numerator,input.getDenominator()),
-				 mult(_denominator, input.getNumerator()));
-		_denominator = mult(_denominator, input.getDenominator());
+		var A = mult(_numerator, input.getDenominator());
+		var B = mult(_denominator, input.getNumerator());
+		if(greater(B,A) || equals(B,A)){
+		    return true;
+		} else{
+		    return false;
+		}
+	    },
+
+	    plus: function(input){
+		_plus(input, false);
+		return this;
+	    },
+
+	    minus: function(input){
+		_plus(input, true);
 		return this;
 	    },
 
@@ -98,15 +181,32 @@ function bigDec(rawVal){
 		input = _isABigDec(input) ? input : bigDec(input);
 		_numerator = mult(_numerator, input.getNumerator());
 		_denominator = mult(_denominator, input.getDenominator());
+		if(_isNegitive == input.isNegitive()){
+		    _isNegitive = false;
+		} else {
+		    _isNegitive = true;
+		}
 		return this;
 	    },
 
-	    isNegitive: function(){
-		return(_isNegitive);
+	    over: function(input){
+		input = _isABigDec(input) ? input : bigDec(input);
+		_numerator = mult(_numerator, input.getDenominator());
+		_denominator = mult(_denominator, input.getNumerator());
+		if(_isNegitive == input.isNegitive()){
+		    _isNegitive = false;
+		} else {
+		    _isNegitive = true;
+		}
+		return this;
+	    },
+
+	    dividedBy: function(input){
+		return(this.over(input));
 	    },
 
 	    isNegitive: function(){
-		return(_isNegitive);
+		return  _isNegitive;
 	    },
 
 	    
--- BigDecTest.js
+++ BigDecTest.js
@@ -72,16 +72,85 @@ var BigDecTest = new YAHOO.tool.TestCase({
 	Assert.isTrue(Seven.times(3).eq(21));
     },
 
+    testBigIntLib_withLargeishNumbers: function(){
+	Assert.isTrue(bigInt2str(int2bigInt(Number.MAX_VALUE,1,1), 10) = Number.MAX_VALUE);
+	var largeNormal = 999999999
+	Assert.isTrue(bigInt2str(int2bigInt(largeNormal,1,1), 10) = Number.MAX_VALUE);
+    },
+
     testLargeNumbers: function(){
 	var bigestNormal = bigDec(Number.MAX_VALUE);
 	Assert.isTrue(bigDec(1).plus(bigestNormal).plus(bigestNormal).eq(bigestNormal.times(2).plus(1)));
 
 	var bigestNormal = bigDec(Number.MAX_VALUE);
 	Assert.isTrue(bigDec(1).plus(bigestNormal).plus(bigestNormal).eq(bigestNormal.times(2).plus(1)));
+
+	var bigestNormal = bigDec(Number.MAX_VALUE);
+	var largeNormal = 999999999
+	var bnT4P1 = bigDec(largeNormal).times(4).plus(1);
+	Assert.isFalse(bigDec(1).plus(largeNormal).plus(largeNormal).eq(bnT4P1));
+
+	var bigestNormal = bigDec(largeNormal);
+//	Assert.isFalse(bigDec(1).plus(bigestNormal).plus(bigestNormal).eq(bigestNormal.times(4).plus(1)));
+    },
+
+    testGT_LT: function(){
+	Assert.isTrue(bigDec(10).gt(5));
+	Assert.isTrue(bigDec(10).gte(5));
+	Assert.isTrue(bigDec(10).gte(5.01));
+	Assert.isTrue(bigDec(10).gte(10));
+
+	Assert.isTrue(bigDec(8).lt(10));
+	Assert.isTrue(bigDec(8.999).lt(10));
+	Assert.isTrue(bigDec(8.999).lte(10));
+	Assert.isTrue(bigDec(10).lte(10));
+
+	Assert.isFalse(bigDec(10).lt(10));
+	Assert.isFalse(bigDec(10).gt(10));
+	Assert.isFalse(bigDec(9).gt(10));
+	Assert.isFalse(bigDec(90).lt(10));
     },
 
     testNegitiveNumbers: function(){
 	var negOne = bigDec(-1);
+	Assert.isTrue(negOne.isNegitive());
+	Assert.isTrue(bigDec(negOne).isNegitive());
+	Assert.isTrue(bigDec("-1").isNegitive());
+    },
+
+    testBigIntNegAddition: function(){
+	Assert.areEqual(1,negative(add(int2bigInt(-5,1,1),int2bigInt(4,1,1))));
+	Assert.areEqual(0,negative(add(int2bigInt(-3,1,1),int2bigInt(4,1,1))));
+    },
+
+    testNegitiveNumbersAddition: function(){
+//	$("#body").append(bigDec(-10).print());
+//	$("#body").append("<BR>");
+//	$("#body").append(bigDec(10).print());
+//	$("#body").append("<BR>");
+//	$("#body").append(bigDec(-10).times(-1).print());
+	Assert.isTrue(bigDec(-10).plus(8).isNegitive());
+	Assert.isFalse(bigDec(-6).plus(8).isNegitive());
+    },
+
+    testNegitiveNumbersTimes: function(){
+	Assert.isTrue(bigDec(-10).times(8).isNegitive());
+	Assert.isFalse(bigDec(-6).times(-8).isNegitive());
+	Assert.isFalse(bigDec(6).times(8).isNegitive());
+    },
+
+    testDivison: function(){
+	Assert.isTrue(bigDec(-10).over(5).eq(-2));
+	Assert.isTrue(bigDec(-10).over(2).eq(-5));
+	Assert.isTrue(bigDec(10).over(200).eq(-.05));
+
+	Assert.isTrue(bigDec(-10).dividedBy(5).eq(-2));
+	Assert.isTrue(bigDec(-10).dividedBy(2).eq(-5));
+	Assert.isTrue(bigDec(10).dividedBy(200).eq(-.05));
+    },
+
+    testDifficultDivison: function(){
+	Assert.isTrue(bigDec(-1).over(3).eq(bigDec(-2).over(6)) );
     },
 
 
